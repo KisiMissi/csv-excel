15e76a9899941578f7d7d48ecc5d5e53405e86a0
Vladislav <grt.cvb@mail.ru>
2023-03-25 12:24:58 +1000

    Adding test

--- src/test/java/org/kaoden/in/FileHandlerTest.java
+++ src/test/java/org/kaoden/in/FileHandlerTest.java
@@ -1,9 +1,16 @@
 package org.kaoden.in;
 
+import junit.framework.Assert;
 import junit.framework.TestCase;
+
+import java.io.File;
 
 public class FileHandlerTest extends TestCase {
 
+    private final static File file =
+            new File("D:\\Projects\\CsvParsing\\src\\main\\resources\\In.csv");;
+
     public void testReadingFile() {
+        Assert.assertNotNull("The returned list must not be empty.", FileHandler.readingFile(file));
     }
 }
